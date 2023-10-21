e189d094548c48500b0432c74ed2b8c53d124a3d
kemper0110 <danil2003.2043@gmail.com>
2023-10-21 23:39:34 +0300

    tournament form

--- frontend/src/routes/TournamentCreate.jsx
+++ frontend/src/routes/TournamentCreate.jsx
@@ -4,9 +4,31 @@ import {Page} from "../components/Page.jsx";
 import {PrimaryButton} from "../components/Button.jsx";
 import {Input} from "../components/Input.jsx";
 import {useForm} from "react-hook-form";
+import {data} from "autoprefixer";
+import {useMutation} from "@tanstack/react-query";
+import {api} from "../api.js";
+import {useNavigate} from "react-router-dom";
 
 export const TournamentCreate = () => {
+    const navigate = useNavigate()
+    const mutation = useMutation(['tournaments'],
+        (data) => api.post("/tournaments/create", data))
+
     const {register, handleSubmit} = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+        const withMatches = {
+            ...data,
+            startDateTime: new Date(),
+            matches: Array.from({length: 3}, () => ({
+                prize: "CRINGE",
+                bestOf: 5
+            })),
+        }
+        mutation.mutateAsync(withMatches).then(() => {
+            navigate('/tournaments')
+        })
+    }
     return (
         <App>
             <Header/>
@@ -16,7 +38,7 @@ export const TournamentCreate = () => {
                         <h1 className={'font-bold text-3xl text-center'}>
                             Создание турнира
                         </h1>
-                        <form className={'mt-8'}>
+                        <form className={'mt-8'} onSubmit={handleSubmit(onSubmit)}>
                             <div className={'flex items-center gap-2'}>
                                 <Input className={'w-full'} placeholder={'Название'} {...register('title')}/>
                                 <PrimaryButton className={'whitespace-nowrap'}>
