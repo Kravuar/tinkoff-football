8a80f4d1d8a037d56b5702e385aa3aa448cdb5c4
kravuar <steam_webmoney@mail.ru>
2023-10-21 23:29:12 +0300

    bebrequest

--- src/main/java/net/kravuar/tinkofffootball/application/web/ExceptionHandlerService.java
+++ src/main/java/net/kravuar/tinkofffootball/application/web/ExceptionHandlerService.java
@@ -1,14 +1,17 @@
 package net.kravuar.tinkofffootball.application.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionHandlerService {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public String handle(Exception exception) {
         return exception.getMessage();
     }
