d207bf88419f82075b07c2891bf8272755fa2b83
kravuar <steam_webmoney@mail.ru>
2023-10-22 12:17:02 +0300

    active init

--- src/main/java/net/kravuar/tinkofffootball/application/repo/TournamentRepo.java
+++ src/main/java/net/kravuar/tinkofffootball/application/repo/TournamentRepo.java
@@ -5,9 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TournamentRepo extends PagingAndSortingRepository<Tournament, Long>, CrudRepository<Tournament, Long> {
     Optional<Tournament> findById(long id);
+
+    List<Tournament> findAllByStatusIs(Tournament.TournamentStatus status);
 }
